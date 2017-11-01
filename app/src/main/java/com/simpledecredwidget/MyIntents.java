aaf5cb2e62aca95245738683fed2f0854393e89c
c-ollins <ocollins444@gmail.com>
2017-11-01 15:19:37 +0100

    completed source code

---
+++ app/src/main/java/com/simpledecredwidget/MyIntents.java
@@ -0,0 +1,27 @@
+package com.simpledecredwidget;
+
+/***************************************************
+ * Copyright 2016 Olanrewaju Collins
+ * Licensed under the Apache License, Version 2.0
+ * (the "License");
+ * you may not use this file except in compliance
+ * with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in
+ * writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied.
+ * See the License for the specific language
+ * governing permissions and
+ * limitations under the License.
+ ****************************************************/
+public class MyIntents {
+    public static String DRAW_STATS ="com.simpledecredwidget.DRAW_STATS";
+    public static String UPDATE ="android.appwidget.action.APPWIDGET_UPDATE";
+    public static String GET_STATS ="com.simpledecredwidget.GET_STATS";
+    public static String DRAW_ERROR ="com.simpledecredwidget.DRAW_ERROR";
+    //public static String DRAW_STATS ="com.simpledecredwidget.DRAW_STATS";
+}
---
+++ app/src/main/java/com/simpledecredwidget/service/DcrStatsService.java
@@ -0,0 +1,74 @@
+package com.simpledecredwidget.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.simpledecredwidget.MyIntents;
+import com.simpledecredwidget.L;
+import com.simpledecredwidget.SimpleDecredWidget;
+
+public class DcrStatsService extends IntentService {
+    private final static DcrStatsUrl DCR_STATS_URL = new DcrStatsUrl("https://dcrstats.com");
+    //private final static DcrStatsUrl DCR_STATS_URL = new DcrStatsUrl("http://10.0.2.2:8090");
+    private final static int TIMEOUT_MS = 10000;
+    private final static int MAX_RETRIES = 0;
+
+    public DcrStatsService() {
+        super("DcrStatsService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        L.l("Service started");
+        getStats();
+    }
+
+    public void sendErrorToWidget() {
+        Intent i = new Intent(this.getApplicationContext(),SimpleDecredWidget.class);
+        i.setAction(MyIntents.DRAW_ERROR);
+        this.getApplicationContext().sendBroadcast(i);
+    }
+
+    public void sendStatsToWidget(DcrStats stats) {
+        Intent i = new Intent(this.getApplicationContext(), SimpleDecredWidget.class);
+        i.setAction(MyIntents.DRAW_STATS);
+        i.putExtra("stats", stats);
+        this.getApplicationContext().sendBroadcast(i);
+    }
+
+    public void getStats() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        L.l("Service sending GET to " + DCR_STATS_URL);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, DCR_STATS_URL.toString(),
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    L.l("Service received non-error response\n" + response);
+                    DcrStats stats = new DcrStats(response);
+                    sendStatsToWidget(stats);
+                }
+            },
+            new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                L.l("Service received error response: ");
+                error.printStackTrace();
+                sendErrorToWidget();
+            }
+        });
+
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                TIMEOUT_MS,
+                MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(stringRequest);
+    }
+}
